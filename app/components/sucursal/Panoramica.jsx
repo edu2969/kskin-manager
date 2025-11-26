49720c2fd2f11f36b9e5d407c72cfc0787ad2c2c
EduTroN <edtronco@gmail.com>
2025-11-26 17:38:48 -0300

    TSConfig y panorámicas
    - iOS drag&drop test

--- app/components/sucursal/Panoramica.jsx
+++ app/components/sucursal/Panoramica.jsx
@@ -335,6 +335,46 @@ export default function Panoramica({ session }) {
         return Date.now() > finEstimado;
     }
 
+    useEffect(() => {
+        function touchHandler(event) {
+            var touches = event.changedTouches,
+                first = touches[0],
+                type = "";
+            switch (event.type) {
+                case "touchstart": type = "mousedown"; break;
+                case "touchmove": type = "mousemove"; break;
+                case "touchend": type = "mouseup"; break;
+                default: return;
+            }
+
+            var simulatedEvent = document.createEvent("MouseEvent");
+            simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                first.screenX, first.screenY,
+                first.clientX, first.clientY, false,
+                false, false, false, 0, null);
+
+            first.target.dispatchEvent(simulatedEvent);
+            event.preventDefault();
+        }
+
+        function initTouchToMouse() {
+            document.addEventListener("touchstart", touchHandler, true);
+            document.addEventListener("touchmove", touchHandler, true);
+            document.addEventListener("touchend", touchHandler, true);
+            document.addEventListener("touchcancel", touchHandler, true);
+        }
+
+        initTouchToMouse();
+
+        // Limpieza al desmontar
+        return () => {
+            document.removeEventListener("touchstart", touchHandler, true);
+            document.removeEventListener("touchmove", touchHandler, true);
+            document.removeEventListener("touchend", touchHandler, true);
+            document.removeEventListener("touchcancel", touchHandler, true);
+        };
+    }, []);
+
     // UI
     return (
         <main className="flex h-screen bg-gradient-to-br from-[#A78D60] via-[#EFC974] to-[#A48A60]">
