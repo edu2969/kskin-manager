310681ae6a2fc1e17516e568e6572bd94a5bf32e
EduTroN <edtronco@gmail.com>
2026-02-06 09:36:38 -0300

    Socket fix

--- lib/socket-client.ts
+++ lib/socket-client.ts
@@ -1,4 +1,6 @@
 "use client";
 
 import io from "socket.io-client";
-export const socket = io();
+export const socket = io(process.env.SOCKET_HOSTNAME || "https://centrointegral.kskin.cl", {
+    transports: ["websocket"]
+});
