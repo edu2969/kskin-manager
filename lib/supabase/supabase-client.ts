c0bc17b990b9b56cf2973f977d33d7db3ab4f42c
EduTroN <edtronco@gmail.com>
2026-02-26 02:23:58 -0300

    Otro test

--- lib/supabase/supabase-client.ts
+++ lib/supabase/supabase-client.ts
@@ -1,34 +1,31 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-function getEnvironmentVariables() {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export function createSupabaseClient(): SupabaseClient {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-    if (!supabaseUrl || !supabaseAnonKey) {
-        throw new Error("Supabase environment variables are not set.");
-    }
-
-    return { supabaseUrl, supabaseAnonKey };
+  return createClient(supabaseUrl, supabaseAnonKey);
 }
 
-export function createSupabaseServerClient() {
-    const { supabaseUrl, supabaseAnonKey } = getEnvironmentVariables();
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const cookieStore = cookies() as any;
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
 
-    return createServerClient(supabaseUrl, supabaseAnonKey, {
-        cookies: {
-            getAll() {
-                return cookieStore.getAll();
-            },
-            setAll(cookiesToSet) {
-                try {
-                    cookiesToSet.forEach(({ name, value, options }) => {
-                        cookieStore.set(name, value, options);
-                    });
-                } catch {}
-            }
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
         }
-    });
+      }
+    }
+  );
 }
